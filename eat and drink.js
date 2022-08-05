b462ee1a2f5223d3b17321719dd4367a1935e92b
TCHOUKEUHilary-M <hilarytchoukeu@gmail.com>
2022-08-05 12:52:17 +0200

    all file updated

---
+++ eat and drink.js
@@ -0,0 +1,19 @@
+const listderepas=document.getElementById('all');
+
+let repas=document.createElement('div');
+repas.classList.add("repas");
+let img=documment.createElement("img");
+img.classList.add('img');
+
+
+
+fetch()
+.then(fumction(response){
+    return response.json()
+})
+.then(function(data){
+    data.forEach(product => {
+        
+    });
+})
+.catch()
